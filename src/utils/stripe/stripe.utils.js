d27ac66a8e5f874a2af12602cec61e0a287d6b8f
yasuladin <yasuhito.sakamoto@gmail.com>
2024-09-29 14:03:00 +0900

    Add await in creating a stripe promise

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe(
+export const stripePromise = await loadStripe(
   import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
 );
